395221338a0b2d5a848a9a7ffaa8d39d117afcd9
maizman <david.maizman@fundacion-jala.org>
2017-09-16 11:44:53 -0400

    añadiendo form2

---
+++ prubaGit/prubaGit/Form2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace prubaGit
+{
+    public partial class Form2 : DevComponents.DotNetBar.Metro.MetroAppForm
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
